132cf11dee055958833c9fefdf059287aa2e0ff6
Marcus Mörck <marcus.morck@gmail.com>
2017-11-07 13:55:29 +0100

    complete

--- form.php
+++ form.php
@@ -1,34 +1,17 @@
 <html>
     <head>
-    <title>form</title>
-
-
+        <title>form</title>
     </head>
     <body>
-
-    <form action="form.php" method="post">
-        <fieldset>
-            <legend>
-                <input type="text" placeholder="text" >
-                <input type="password" placeholder="text">
-                <input type="checkbox" name="favoritarstid" id="sommar" value="sommar"/>
-                <input type="submit" value="submit">
-            </legend>
-        </fieldset>
-    </form>
-
-
-
-
+        <form action="form.php" method="post">
+            <fieldset>
+                <legend>
+                    <input type="text" placeholder="text" >
+                    <input type="password" placeholder="text">
+                    <input type="checkbox"/>
+                    <input type="submit" value="submit">
+                </legend>
+            </fieldset>
+        </form>
     </body>
 </html>
-
-
-
-<?php
-/**
- * Created by PhpStorm.
- * User: marcus
- * Date: 2017-11-07
- * Time: 10:29
- */
---
+++ internal-server-error.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Internal Server Error</title>
+    </head>
+    <body>
+    <h1>Internal Server Error</h1>
+
+    </body>
+</html>
+
+
+<?php
